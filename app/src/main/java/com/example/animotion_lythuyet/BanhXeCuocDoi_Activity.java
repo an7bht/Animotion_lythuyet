6aff55ab635cffffab5f294976ee9d0d5f7f27e2
AnDang <81478990+an7bht@users.noreply.github.com>
2021-09-17 13:13:32 +0700

    lan1

---
+++ app/src/main/java/com/example/animotion_lythuyet/BanhXeCuocDoi_Activity.java
@@ -0,0 +1,14 @@
+package com.example.animotion_lythuyet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BanhXeCuocDoi_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_banh_xe_cuoc_doi);
+    }
+}
